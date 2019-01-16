a6b18df180dbce351b7cff67cbec0b524774ddc3
Inhyuk Na <inhyukna@Inhyukui-iMac-Pro.local>
2019-01-16 21:35:55 +0900

    add test mode

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from time import sleep
 import json
 from bs4 import BeautifulSoup
 import re
-
 from selenium import webdriver
 
 
@@ -33,6 +32,7 @@ def get_postech_menu(menu_obj: Dict) -> Dict:
         i += 1
     menu_obj["stores"][4] = obj
     return menu_obj
+
 
 def get_food_court_menu(menu_obj: Dict) -> Dict:
     url = "http://fd.postech.ac.kr/bbs/board.php?bo_table=food_court"
@@ -61,19 +61,21 @@ def get_food_court_menu(menu_obj: Dict) -> Dict:
     menu_obj["stores"][5] = obj
     return menu_obj
 
-def post_slackbot(slack_token: str, menu_obj: Dict) -> None:
-    slack = Slacker(slack_token)
 
-    slack.chat.post_message('#밥', '★오늘의 식단을 소개합니다★')
+def post_slackbot(slack_token: str, menu_obj: Dict, test: bool=False) -> None:
+    slack = Slacker(slack_token)
+    channel_name = "#밥_dev" if test else "#밥"
+
+    slack.chat.post_message(channel_name, '★오늘의 식단을 소개합니다★')
 
     inje_obj = menu_obj["stores"][2]
-    slack.chat.post_message('#밥', '♥인재개발원♥-아침-'+inje_obj["menus"][0]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '♥인재개발원♥-아침-'+inje_obj["menus"][0]["name"] + ":::" +
                             inje_obj["menus"][0]["description"])
-    slack.chat.post_message('#밥', '♥인재개발원♥-점심-'+inje_obj["menus"][1]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '♥인재개발원♥-점심-'+inje_obj["menus"][1]["name"] + ":::" +
                             inje_obj["menus"][1]["description"])
-    slack.chat.post_message('#밥', '♥인재개발원♥-점심-'+inje_obj["menus"][2]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '♥인재개발원♥-점심-'+inje_obj["menus"][2]["name"] + ":::" +
                             inje_obj["menus"][2]["description"])
-    slack.chat.post_message('#밥', '♥인재개발원♥-저녁-'+inje_obj["menus"][3]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '♥인재개발원♥-저녁-'+inje_obj["menus"][3]["name"] + ":::" +
                             inje_obj["menus"][3]["description"])
 
     gasok_obj = menu_obj["stores"][3]
@@ -81,45 +83,45 @@ def post_slackbot(slack_token: str, menu_obj: Dict) -> None:
     with open(tmp_file_loc, 'wb') as f:
         resp = requests.get(gasok_obj["menus"][0]["description"], verify=False)
         f.write(resp.content)
-    slack.chat.post_message('#밥', "아래는 가속기연구소 식단입니다.")
+    slack.chat.post_message(channel_name, "아래는 가속기연구소 식단입니다.")
     slack.files.upload(tmp_file_loc, channels='#밥', title="가속기연구소 식단")
 
     # POSTECH 학생 식당
-    slack.chat.post_message('#밥', "아래는 POSTECH 학생식당 식단입니다.")
+    slack.chat.post_message(channel_name, "아래는 POSTECH 학생식당 식단입니다.")
     student_obj = menu_obj["stores"][4]
-    slack.chat.post_message('#밥', '학생식당-아침 A-'+student_obj["menus"][0]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '학생식당-아침 A-'+student_obj["menus"][0]["name"] + ":::" +
                             student_obj["menus"][0]["description"])
-    slack.chat.post_message('#밥', '학생식당-아침 B-'+student_obj["menus"][1]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '학생식당-아침 B-'+student_obj["menus"][1]["name"] + ":::" +
                             student_obj["menus"][1]["description"])
-    slack.chat.post_message('#밥', '학생식당-점심-'+student_obj["menus"][2]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '학생식당-점심-'+student_obj["menus"][2]["name"] + ":::" +
                             student_obj["menus"][2]["description"])
-    slack.chat.post_message('#밥', '학생식당-저녁-'+student_obj["menus"][3]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '학생식당-저녁-'+student_obj["menus"][3]["name"] + ":::" +
                            student_obj["menus"][3]["description"])
 
     # POSTECH 교직원 식당
-    slack.chat.post_message('#밥', "아래는 POSTECH 교직원식당 식단입니다.")
+    slack.chat.post_message(channel_name, "아래는 POSTECH 교직원식당 식단입니다.")
     educational_personnel_obj = menu_obj["stores"][4]
-    slack.chat.post_message('#밥', '교직원식당-아침 A-'+educational_personnel_obj["menus"][4]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '교직원식당-아침 A-'+educational_personnel_obj["menus"][4]["name"] + ":::" +
                             educational_personnel_obj["menus"][4]["description"])
 
     # POSTECH 푸드코트
-    slack.chat.post_message('#밥', "아래는 POSTECH 푸드코트 식단입니다.")
+    slack.chat.post_message(channel_name, "아래는 POSTECH 푸드코트 식단입니다.")
     foodcourt_obj = menu_obj["stores"][5]
-    slack.chat.post_message('#밥', '푸드코트-아침(한식)-'+foodcourt_obj["menus"][0]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '푸드코트-아침(한식)-'+foodcourt_obj["menus"][0]["name"] + ":::" +
                             foodcourt_obj["menus"][0]["description"])
-    slack.chat.post_message('#밥', '푸드코트-중식(한식)-'+foodcourt_obj["menus"][1]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '푸드코트-중식(한식)-'+foodcourt_obj["menus"][1]["name"] + ":::" +
                             foodcourt_obj["menus"][1]["description"])
-    slack.chat.post_message('#밥', '푸드코트-중식(양식)-'+foodcourt_obj["menus"][2]["name"] + ":::" +
+    slack.chat.post_message(channel_name, '푸드코트-중식(양식)-'+foodcourt_obj["menus"][2]["name"] + ":::" +
                             foodcourt_obj["menus"][2]["description"])
 
     # RIST
-    slack.chat.post_message('#밥', "아래는 POSTECH RIST 식단입니다.")
+    slack.chat.post_message(channel_name, "아래는 POSTECH RIST 식단입니다.")
     rist_obj = menu_obj["stores"][6]
-    slack.chat.post_message('#밥', 'RIST-아침-'+rist_obj["menus"][0]["name"] + ":::" +
+    slack.chat.post_message(channel_name, 'RIST-아침-'+rist_obj["menus"][0]["name"] + ":::" +
                             rist_obj["menus"][0]["description"])
-    slack.chat.post_message('#밥', 'RIST-중식-'+rist_obj["menus"][1]["name"] + ":::" +
+    slack.chat.post_message(channel_name, 'RIST-중식-'+rist_obj["menus"][1]["name"] + ":::" +
                             rist_obj["menus"][1]["description"])
-    slack.chat.post_message('#밥', 'RIST-석식-'+rist_obj["menus"][2]["name"] + ":::" +
+    slack.chat.post_message(channel_name, 'RIST-석식-'+rist_obj["menus"][2]["name"] + ":::" +
                             rist_obj["menus"][2]["description"])
 
 
@@ -139,8 +141,7 @@ def get_rist_menu(browser, menu_obj: Dict) -> Dict:
     time.sleep(3)
     obj = dict()
     obj["menus"] = dict()
-    for idx in range(1,4):
-        #print (idx)
+    for idx in range(1, 4):
         login_attempt = browser.find_element_by_xpath('//*[@id="mealType"]/li[' + str(idx) + ']/a')
         login_attempt.click()
         time.sleep(3)
@@ -154,21 +155,31 @@ def get_rist_menu(browser, menu_obj: Dict) -> Dict:
     menu_obj["stores"].append(obj)
     return menu_obj
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Gets tokens')
     parser.add_argument("slack_token", help="slack token", type=str)
     parser.add_argument("boblab_token", help="boblab token", type=str)
+    parser.add_argument("--test", help="if specified, run for test")
     args = parser.parse_args()
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
     browser = webdriver.Chrome(chrome_options=chrome_options)
 
-    while True:
-        if time.localtime().tm_hour == 9:
-            menu_obj = get_menu(args.boblab_token)
-            menu_obj = get_postech_menu(menu_obj)
-            menu_obj = get_food_court_menu(menu_obj)
-            menu_obj = get_rist_menu(browser, menu_obj)
-            post_slackbot(args.slack_token, menu_obj)
-        sleep(3600)
+    if args.test:
+        test = True
+        menu_obj = get_menu(args.boblab_token)
+        menu_obj = get_postech_menu(menu_obj)
+        menu_obj = get_food_court_menu(menu_obj)
+        menu_obj = get_rist_menu(browser, menu_obj)
+        post_slackbot(args.slack_token, menu_obj, test)
+    else:
+        while True:
+            if time.localtime().tm_hour == 9:
+                menu_obj = get_menu(args.boblab_token)
+                menu_obj = get_postech_menu(menu_obj)
+                menu_obj = get_food_court_menu(menu_obj)
+                menu_obj = get_rist_menu(browser, menu_obj)
+                post_slackbot(args.slack_token, menu_obj)
+            sleep(3600)
